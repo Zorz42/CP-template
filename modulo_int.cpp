5048f839e6f45f52a3012a76ee695233a97cde4b
Jakob Schrader <jakob.schrader@guest.arnes.si>
2025-04-01 16:17:14 +0200

    Add fast inverse with precompute to modulo int struct

--- modulo_int.cpp
+++ modulo_int.cpp
@@ -1,6 +1,12 @@
+#define MINVPRECOMP 10000000
+typedef long long ll;
+
 const int MOD=1e9+7;
 struct M{
     ll val=0;
+		static ll inv[MINVPRECOMP+1];
+		static ll calc_inv(ll i){ if(i<=MINVPRECOMP) return inv[i]; return (MOD-MOD/i)*calc_inv(MOD%i)%MOD; }
+		static void precomp(){ inv[1]=1; for(ll i=2;i<=MINVPRECOMP;++i){ inv[i]=(MOD-MOD/i)*inv[MOD%i]%MOD; if(inv[i]<0) inv[i]+=MOD; } }
     M(ll x){val=(x%MOD+MOD)%MOD;}
     M()=default;
     template<class T>M operator+(T bt){M b(bt);M r(val);r.val+=b.val;if(r.val>=MOD)r.val-=MOD;return r;}
@@ -10,8 +16,11 @@ struct M{
     template<class T>M&operator-=(T bt){M b(bt);*this=*this-b;return*this;}
     template<class T>M&operator*=(T bt){M b(bt);*this=*this*b;return*this;}
     M pow(ll e){M r(1);M a(val);while(e){if(e%2)r=r*a;a=a*a;e/=2;}return r;}
-    template<class T>M operator/(T bt){M b(bt);M r(val);r*=b.pow(MOD-2).val;return r;}
-    template<class T>M&operator/=(T bt){M b(bt);*this=*this/b;return*this;}
+
+    template<class T>M operator/(T bt){ M b(bt); return M(val*M::calc_inv(b.val)); }
+    template<class T>M&operator/=(T bt){M b(bt);*this*=M::calc_inv(b.val);return*this;}
 };
 ostream&operator<<(ostream&s,M m){s<<m.val;return s;}
 istream&operator>>(istream&s,M&m){s>>m.val;return s;}
+
+//	Add `M::precomp();` to start of main

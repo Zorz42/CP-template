d084eb646eae36ca4c221b493a848c1c7c2c04cd
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2026-03-02 12:57:12 +0100

    cleaned up suffix array and hash string

--- algos/hash_string.cpp
+++ algos/hash_string.cpp
@@ -1,31 +1,28 @@
-
-const int HMOD=1e9+7;
-const int HX=31;
-
-class FastString{
-    string str;
-    vector<ll>hashes;
-    vector<ll>pows;
-    int len;
-public:
-    FastString(string str):str(str){
-        len=(int)str.size();
-        hashes.resize(len+1);
-        pows.resize(len+1);
-        pows[0]=1;
-        for(int i=1;i<=len;i++)
-            pows[i]=(pows[i-1]*HX)%HMOD;
-        
-        hashes[0]=0;
-        for(int i=1;i<=len;i++)
-            hashes[i]=(hashes[i-1]+pows[i-1]*(str[i-1]-'a'+1))%HMOD;
-    }
-    
-    ll get(int l,int r){
-        ll hsh=hashes[r]+HMOD-hashes[l];
-        hsh%=HMOD;
-        hsh*=pows[len-l];
-        hsh%=HMOD;
-        return hsh;
-    }
+const int HMOD1=1e9+7;
+const int HMOD2=1e9+9;
+const int HX1=31;
+const int HX2=29;
+ 
+struct FastString{
+	string s;
+	vc<ll>h1,h2,p1,p2;
+	FastString(string s):s(s){
+		p1={1};
+		fwd(i,1,sz(s)+1)
+			p1.pb((p1[i-1]*HX1)%HMOD1);
+		p2={1};
+		fwd(i,1,sz(s)+1)
+			p2.pb((p2[i-1]*HX2)%HMOD2);
+		h1={1};
+		fwd(i,1,sz(s)+1)
+			h1.pb((h1[i-1]+p1[i-1]*(s[i-1]-'a'+1))%HMOD1);
+		h2={1};
+		fwd(i,1,sz(s)+1)
+			h2.pb((h2[i-1]+p2[i-1]*(s[i-1]-'a'+1))%HMOD2);
+	}
+	ll get(int l,int r){
+		ll hsh1=(h1[r]+HMOD1-h1[l])%HMOD1*p1[sz(s)-l]%HMOD1;
+		ll hsh2=(h2[r]+HMOD2-h2[l])%HMOD2*p1[sz(s)-l]%HMOD2;
+		return hsh1+(hsh2<<32);
+	}
 };

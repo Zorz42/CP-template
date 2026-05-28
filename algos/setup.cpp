2e887227c90d7ef399cc5c2b6bf96fb2d4281fe3
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2026-05-28 14:16:12 +0200

    improve setup.cpp

--- algos/setup.cpp
+++ algos/setup.cpp
@@ -4,30 +4,19 @@ setxkbmap us -option caps:escape
 
 ~/.vimrc
 
-set number
-set relativenumber
+set number relativenumber tabstop=2 shiftwidth=2 autoindent
 syntax enable
-set autoindent
-set tabstop=2
-set shiftwidth=2
-set wrap
-set scrolloff=20
 filetype plugin indent on
-autocmd FileType cpp setlocal cindent
 
-Makefile
-
-CXX = g++
-CXXFLAGS = -std=c++20 -O2 -Wall -DLOC
+Makefile:
 
 %: %.cpp
-	$(CXX) $(CXXFLAGS) $< -o $@
+	g++ -std=c++20 -O2 -Wall -DLOC $< -o $@
 
  */
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef long double ld;
 #define fwd(i,a,b) for(int i=(a); i<(b);i++)
 #define rep(i,n) fwd(i,0,n)
 #define all(x) (x).begin(), (x).end()
@@ -36,8 +25,8 @@ typedef long double ld;
 #define f2 second
 #define pb push_back
 #define vc vector
-typedef vector<int> vi;
-typedef vector<ll> vl;
+typedef vc<int> vi;
+typedef vc<ll> vl;
 typedef pair<int,int> pii;
 typedef pair<ll,ll> pll;
 
@@ -67,7 +56,7 @@ void solve(){
 
 int main(){
 	cin.tie(0);cout.tie(0);ios::sync_with_stdio(false);
-	int t=1;cin>>t;
+	int t=1;//cin>>t;
 	while(t--)solve();
 	return 0;
 }
